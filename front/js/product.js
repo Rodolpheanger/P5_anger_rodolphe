acf0a36c3f1ef622d8310a997cdeb424cb8cbdd6
Rodolphe Anger <rodolphe.anger@yahoo.fr>
2022-02-22 11:40:11 +0100

    Refactorisation code panier pendant session mentorat

--- front/js/product.js
+++ front/js/product.js
@@ -2,13 +2,13 @@
 
 /** récupération de l'id produit dans l'URL */
 const getItemId = () => {
-  return (itemId = new URL(window.location.href).searchParams.get("id"));
+  return new URL(window.location.href).searchParams.get("id");
 };
 
 /** fonction fetch pour récupérer les données de l'item dans l'API via son id */
 const fetchItemData = async () => {
   try {
-    getItemId();
+    const itemId = getItemId();
     const response = await fetch(
       "http://localhost:3000/api/products/" + itemId
     );
@@ -19,48 +19,48 @@ const fetchItemData = async () => {
 };
 
 /** Fonction d'affichage des divers éléments de la page */
-const productDisplay = async () => {
+const productPageInit = async () => {
   const item = await fetchItemData();
-  pageTitle(item);
-  itemImage(item);
-  itemName(item);
-  itemPrice(item);
-  itemDescription(item);
-  itemColors(item);
+  pageTitle(item.name);
+  itemImage(item.imageUrl, item.altTxt);
+  itemName(item.name);
+  itemPrice(item.price);
+  itemDescription(item.description);
+  itemColors(item.colors);
+  addToCartInit();
 };
-productDisplay();
 
 /**Ajoute le nom du produit dans la balise <title> de la page*/
-const pageTitle = (item) => {
-  document.title = item.name;
+const pageTitle = (itemName) => {
+  document.title = itemName;
 };
 
 // /** ajout de l'image dans le DOM */
-const itemImage = (item) => {
+const itemImage = (itemUrl, itemAltTxt) => {
   let image = document.createElement("img");
-  image.setAttribute("src", item.imageUrl);
-  image.setAttribute("alt", item.altTxt);
+  image.setAttribute("src", itemUrl);
+  image.setAttribute("alt", itemAltTxt);
   document.querySelector(".item__img").appendChild(image);
 };
 
 // /** ajout du nom dans le DOM */
-const itemName = (item) => {
-  document.getElementById("title").textContent = item.name;
+const itemName = (itemName) => {
+  document.getElementById("title").textContent = itemName;
 };
 
 /** ajout du prix dans le DOM */
-const itemPrice = (item) => {
-  document.getElementById("price").textContent = item.price;
+const itemPrice = (itemPrice) => {
+  document.getElementById("price").textContent = itemPrice;
 };
 
 // /** ajout de la description dans le DOM */
-const itemDescription = (item) => {
-  document.getElementById("description").textContent = item.description;
+const itemDescription = (itemDescription) => {
+  document.getElementById("description").textContent = itemDescription;
 };
 
 /** ajout des couleurs dans le DOM */
-const itemColors = (item) => {
-  for (let color of item.colors) {
+const itemColors = (itemColors) => {
+  for (let color of itemColors) {
     let colorOption = new Option(color, color);
     document.getElementById("colors").appendChild(colorOption);
   }
@@ -69,13 +69,18 @@ const itemColors = (item) => {
 // ---------------------------- Ajout au panier --------------------------------------------
 
 /** Execute les fonctions necessaires au click sur le bouton "ajouter au panier" */
-document.getElementById("addToCart").addEventListener("click", () => {
-  const itemNameContainer = document.getElementById("title");
-  const itemColorsContainer = document.getElementById("colors");
-  const itemQuantityContainer = document.getElementById("quantity");
-  getItemId();
-  checkValidity(itemColorsContainer, itemNameContainer, itemQuantityContainer);
-});
+const addToCartInit = () => {
+  document.getElementById("addToCart").addEventListener("click", () => {
+    const itemNameContainer = document.getElementById("title");
+    const itemColorsContainer = document.getElementById("colors");
+    const itemQuantityContainer = document.getElementById("quantity");
+    checkValidity(
+      itemColorsContainer,
+      itemNameContainer,
+      itemQuantityContainer
+    );
+  });
+};
 
 /** Vérification que la couleur et la quantité sont bien renseignées avant validation */
 const checkValidity = (
@@ -89,61 +94,71 @@ const checkValidity = (
     );
   } else if (itemQuantityContainer.value == 0) {
     alert(
-      `Veuillez indiquer le nombre de "${itemNameContainer.textContent}" que vous souhaitez ajouter au panier`
+      `Veuillez indiquer le nombre de "${itemNameContainer.textContent}" ${itemColorsContainer} que vous souhaitez ajouter au panier`
     );
   } else {
-    addToCart(itemId, itemColorsContainer, itemQuantityContainer);
+    addToCart(itemColorsContainer, itemQuantityContainer);
     validation(itemQuantityContainer, itemNameContainer, itemColorsContainer);
   }
 };
 
+//*************************************************************************************************
+//*************************************************************************************************
+//******************** Voir pour que getLocalStorage renvoi un tableau ****************************
+//******************** et pour supprimer if (localStorageData === null)  **************************
+//******************* {localStorageData = [];******************************************************
+//*************************************************************************************************
+//*************************************************************************************************
+
 /** Fonction qui recupère le contenu du local storage */
 const getLocalStorage = () => {
-  return (localStorageData = JSON.parse(window.localStorage.getItem("cart")));
+  return JSON.parse(window.localStorage.getItem("cart"));
 };
 
 /** Fonction pour envoyer le contenu du choix utilisateur dans le local storage */
-const setLocalStorage = () => {
-  localStorageData = JSON.stringify(localStorageData);
-  window.localStorage.setItem("cart", localStorageData);
+const setLocalStorage = (cartData) => {
+  const cartDataToStringnify = JSON.stringify(cartData);
+  window.localStorage.setItem("cart", cartDataToStringnify);
 };
 
 /** Fonction qui récupère les choix utilisateur, les compare avec le local storage pour le modifier si besoin */
-const addToCart = (itemId, itemColorsContainer, itemQuantityContainer) => {
-  getLocalStorage(itemId, itemColorsContainer);
+const addToCart = (itemColorsContainer, itemQuantityContainer) => {
+  const itemId = getItemId();
+  let localStorageData = getLocalStorage(itemId, itemColorsContainer);
+  const newProduct = {
+    id: itemId,
+    color: itemColorsContainer.value,
+    quantity: itemQuantityContainer.value,
+  };
   if (localStorageData === null) {
-    localStorageData = [
-      {
-        id: itemId,
-        color: itemColorsContainer.value,
-        quantity: itemQuantityContainer.value,
-      },
-    ];
-    setLocalStorage();
-  } else if (localStorageData != null) {
-    for (let entry of localStorageData) {
-      if (entry.id === itemId && entry.color === itemColorsContainer.value) {
-        return (
-          (entry.quantity = itemQuantityContainer.value), setLocalStorage()
-        );
-      }
-    }
-    for (let entry of localStorageData) {
-      if (
-        (entry.id === itemId && entry.color !== itemColorsContainer.value) ||
-        entry.id !== itemId
-      ) {
-        return (
-          localStorageData.push({
-            id: itemId,
-            color: itemColorsContainer.value,
-            quantity: itemQuantityContainer.value,
-          }),
-          setLocalStorage()
-        );
-      }
+    localStorageData = [];
+  }
+  createOrModifyEntry(localStorageData, itemId, newProduct);
+};
+
+/** Fonction qui compare puis modifie ou ajoute une entrée dans le tableau du local storage */
+const createOrModifyEntry = (localStorageData, itemId, newProduct) => {
+  for (let entry of localStorageData) {
+    /** Si même id et même couleur existe: modification quantité */
+    if (entry.id === itemId && entry.color === newProduct.color) {
+      return (
+        (entry.quantity = newProduct.quantity),
+        setLocalStorage(localStorageData)
+      );
     }
   }
+  /** Sinon push une nouvelle entrée */
+  newEntryPush(localStorageData, newProduct);
+};
+
+/** Fonction pour push le choix de l'utilisateur dans le tableau du local storage */
+const newEntryPush = (localStorageData, productToPush) => {
+  localStorageData.push({
+    id: productToPush.id,
+    color: productToPush.color,
+    quantity: productToPush.quantity,
+  }),
+    setLocalStorage(localStorageData);
 };
 
 /** Pop-up de confirmation du modèle, de la couleur et de la quantité du produit ajouté au panier + demande redirection vers acceuil ou panier */
@@ -162,3 +177,5 @@ const validation = (
     window.location.href = "../html/index.html";
   }
 };
+
+productPageInit();
